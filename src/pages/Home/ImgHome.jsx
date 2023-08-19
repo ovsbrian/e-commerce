9267742d46a1f25b102a6625fb69c8521302a560
ovsbrian <ovsbrian@gmail.com>
2023-08-18 21:45:42 -0300

    change bg in home

--- src/pages/Home/ImgHome.jsx
+++ src/pages/Home/ImgHome.jsx
@@ -12,12 +12,12 @@ export const ImgHome = () => {
   }
   return (
     <>
-      <div className="pt-24 flex-col md:flex-row md:h-96 md:gap-36 bg-custom flex items-center justify-center ">
+      <div className="pt-24 flex-col md:flex-row md:h-96 md:gap-36 bg-custom flex items-center md:pb-4 justify-center bg-[#F6F6F6] ">
         <div className="flex flex-col justify-center items-center h-full">
           <span className="text-4xl font-bold">{products[0].name}</span>
         </div>
 
-        <div className=" w-full md:w-80 mt-2 bg-slate-400 h-80 md:h-full">
+        <div className=" w-full md:w-80 mt-2 md:mt-0 bg-slate-400 h-80 md:h-full">
           <img className="h-full w-full" src={products[0].imageURL} alt="" />
         </div>
       </div>
